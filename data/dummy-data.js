291018f1f464f96c216a3c8122301776e0b2a456
mojahid-belaman <mojahidbelaman@gmail.com>
2023-03-22 11:59:05 +0000

    add colors to use in app

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -4,14 +4,14 @@ import { Meal } from "../models/meal";
 export const Categories = [
   new Category("c1", "Italian", "#f5428d"),
   new Category("c2", "Quick & Easy", "#f54242"),
-  new Category("c3", "Hamburgers", "#f5428d"),
-  new Category("c4", "German", "#f5428d"),
-  new Category("c5", "Light & Lovely", "#f5428d"),
-  new Category("c6", "Exotic", "#f5428d"),
-  new Category("c7", "Breakfast", "#f5428d"),
-  new Category("c8", "Asian", "#f5428d"),
-  new Category("c9", "French", "#f5428d"),
-  new Category("c10", "Summer", "#f5428d"),
+  new Category("c3", "Hamburgers", "#f5a442"),
+  new Category("c4", "German", "#f5d142"),
+  new Category("c5", "Light & Lovely", "#368dff"),
+  new Category("c6", "Exotic", "#41d95d"),
+  new Category("c7", "Breakfast", "#9eecff"),
+  new Category("c8", "Asian", "#b9ffb0"),
+  new Category("c9", "French", "#ffc7ff"),
+  new Category("c10", "Summer", "#47fced"),
 ];
 
 export const Meals = [
