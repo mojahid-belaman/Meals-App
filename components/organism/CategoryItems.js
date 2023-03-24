54df64983f069f4144a3c561e9ad528b1f943b1c
mojahid-belaman <mojahidbelaman@gmail.com>
2023-03-24 17:39:17 +0000

    create component CategoryItems

---
+++ components/organism/CategoryItems.js
@@ -0,0 +1,15 @@
+import { Pressable, View } from "react-native";
+
+const CategoryItems = ({ title, color }) => {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default CategoryItems;
