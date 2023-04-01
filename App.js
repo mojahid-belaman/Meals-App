650c4878120f9bf7aa34d4b2b6f706d72cefd868
mojahid-belaman <mojahidbelaman@gmail.com>
2023-04-01 17:03:29 +0000

    add thirty part library name react-navigation

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from "react-native";
 import CategoryScreen from "./screens/CategoryScreen";
+import MealsScreen from "./screens/MealsScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle={'default'}/>
-      <CategoryScreen />
+      <StatusBar barStyle={"default"} />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="MealCategory"
+            component={CategoryScreen}
+          ></Stack.Screen>
+          <Stack.Screen name="Meal" component={MealsScreen}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
